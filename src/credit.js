5f5fc7121319ab290e9563ed0effc5a182e297e7
shubhankar.singh <shubhankar.singh@zeeve.io>
2023-08-28 18:24:30 +0530

    ft:4 commit

--- src/credit.js
+++ src/credit.js
@@ -2,6 +2,7 @@
 
 const acceptOpenAIKey = () => {
     const template = HtmlService.createTemplateFromFile('index-key');
+    const RegisteredEmail = Session.getActiveUser().getEmail();
     const html = template.evaluate().setTitle('OpenAI-Key');
     DocumentApp.getUi().showSidebar(html);
 }
